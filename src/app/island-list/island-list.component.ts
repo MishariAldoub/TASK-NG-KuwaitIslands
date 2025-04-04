21aac0a7a08ce2536fbba1275eed6d027808ea8c
Octowl <alsaff1987@gmail.com>
2025-04-04 11:05:34 +0300

    add island list component

---
+++ src/app/island-list/island-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-island-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './island-list.component.html',
+  styleUrl: './island-list.component.css',
+})
+export class IslandListComponent {}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import islands from '../../data/islands';
 import { HeaderComponent } from '../header/header.component';
+import { IslandListComponent } from '../island-list/island-list.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, IslandListComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
