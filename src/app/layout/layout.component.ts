381d6a33c131166cb942a90033351701ac08840d
Octowl <alsaff1987@gmail.com>
2025-04-04 10:17:39 +0300

    add header

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import islands from '../../data/islands';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {
